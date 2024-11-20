427024b4dbc981b06311bcdea3a77f92853aa686
roge111 <64649951+roge111@users.noreply.github.com>
2024-11-20 23:17:42 +0300

    Добавление новых знаний

--- src/firstGo/firstProgramm/main.go
+++ src/firstGo/firstProgramm/main.go
@@ -5,5 +5,17 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	//Тренировка на запись в переменную
+	var a = 7
+	var b = -8.0
+	var c = "Hello"
+	var d = a
+	//Тренировка на изменение и вывод
+	fmt.Println(a)
+	a += 1
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	
 }

ba9e56d607849f70bb20cc492e145bae98035d04
roge111 <64649951+roge111@users.noreply.github.com>
2024-11-28 11:57:32 +0300

    Добавлена работа с функциями, создана функция input()

--- src/firstGo/firstProgramm/main.go
+++ src/firstGo/firstProgramm/main.go
@@ -2,20 +2,46 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
 )
+//На вход поступает три строки, вывести в том же порядке
+func input()string{
+	scanner := bufio.NewScanner(os.Stdin)
 
-func main() {
-	//Тренировка на запись в переменную
-	var a = 7
-	var b = -8.0
-	var c = "Hello"
-	var d = a
-	//Тренировка на изменение и вывод
-	fmt.Println(a)
-	a += 1
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
-	fmt.Println(d)
+	_ = scanner.Scan()
+	result:= scanner.Text()
+	return result
+}
+
+
+
+
+func practic1() {
+	str1 := input()
+	str2:= input()
+	str3 := input()
+
+	fmt.Println(str1)
+	fmt.Println(str2)
+	fmt.Println(str3)
 	
+}
+
+
+
+func practic2(){
+	split := input()
+	string1 := input()
+	string2 := input()
+	string3 := input()
+	// fmt.Print(string1, split, string2, split, string3) //Вариант решения раз
+
+	result := string1 + split + string2 + split + string3 // Вариант решения 2
+	fmt.Println(result)
+}
+
+func main(){
+	practic1()
+	practic2()
 }
